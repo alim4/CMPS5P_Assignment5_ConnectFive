dc0851f2eb566efb0bda31c1d78960afbf1abd6d
alim4 <theineffablebob@gmail.com>
2014-05-13 11:01:18 -0700

    Add input checking, row and column win checks

--- ConnectFive.py
+++ ConnectFive.py
@@ -1,3 +1,5 @@
+import sys
+
 __author__ = 'anthonylim'
 
 # Anthony Lim
@@ -22,25 +24,45 @@ def main():
 
     while True:
         if playerX:
-            playerX_input = raw_input("Player X - Choose a column: ")
-            if 0 > int(playerX_input) > 8:
-                raise ValueError("Input must be between integers 0 and 8")
+            playerX_input = get_input("X", 10)
+            if playerX_input == "skip":
+                playerX = False
+                playerO = True
             else:
                 place_piece("x", myboard, int(playerX_input))
-                scan_board(myboard)
                 print_board(myboard)
+                scan_board(myboard)
                 playerX = False
                 playerO = True
         if playerO:
-            playerO_input = raw_input("Player O - Choose a column: ")
-            if 0 > int(playerO_input) > 8:
-                raise ValueError("Input must be between integers 0 and 8")
-            else:
-                place_piece("o", myboard, int(playerO_input))
-                scan_board(myboard)
-                print_board(myboard)
+            playerO_input = get_input("O", 10)
+            if playerX_input == "skip":
                 playerX = True
                 playerO = False
+            else:
+                place_piece("o", myboard, int(playerO_input))
+                print_board(myboard)
+                scan_board(myboard)
+                playerX = True
+                playerO = False
+
+def get_input(player, num_tries):
+    itor = 0
+    while itor < num_tries:
+        input = raw_input("Player {0} - Choose a column: ".format(player))
+        if not input.isdigit():
+            itor += 1
+            print "Bad input. Try again."
+            continue
+        if int(input) in range(9):
+            return input
+        else:
+            print "Bad input. Try again."
+            itor += 1
+
+    # if player tries too many times, skip turn
+    print "PLAYER {0}'s TURN SKIPPED".format(player)
+    return "skip"
 
 def print_board(board):
     # print top row
@@ -65,22 +87,72 @@ def place_piece(piece, board, column):
     return board
 
 def scan_board(board):
-    # Horizontal
-    for row in board:
-        print row
-        itor = 0
-        prev = ''
-        for val in row:
-            prev = val
-            
-            print val
+    letters = ["x", "o"]
 
-    # Vertical
+    # For X and O
+    for letter in letters:
+        # Horizontal
+        #print "Row"
+        for row in board:
+            if row.count(letter) >= 5:
+                if check("row", row, letter):
+                    win_game(letter)
+            #print "{0} ~ {1}".format(row, row.count("x"))
+
+        # Vertical
+        #print "Column"
+        for column in range(len(board)):
+            col = [row[column] for row in board]
+            if col.count(letter) >= 5:
+                if check("col", col, letter):
+                    win_game(letter)
+            #print "{0} ~ {1}".format(col, col.count("x"))
 
     # Diagonal Right
+    for i in range(7):
+        for j in range(9):
+            for k in range(18):
+                if j == i-9+k:
+                    board[i][j] = chr(j+100).upper()
+
+
+    print print_board(board)
+
+
+    # col[0][0]
+    # col[0][1] [1][0]
+    # col[0][2] [1][1] [2][0]
+    # col[0][3] [1][2] [2][1] [3][0]
 
     # Diagonal Left
     return
+
+def win_game(player):
+    print "Player {0} has won the game!".format(player.upper())
+    sys.exit(0)
+
+def check(type, row, letter):
+    # if itor is 5 for 5 consecutive
+    # consider a successful match
+    itor = 0
+    is_winner_found = False
+
+    if type == "row":
+        rangenum = 4
+    elif type == "col":
+        rangenum = 3
+
+    for i in range(rangenum):
+        for j in range(5):
+            if row[i+j] != letter:
+                is_winner_found = False
+                break
+            else:
+                is_winner_found = True
+            if j == 4 and is_winner_found:
+                return True
+
+    return False
 
 main()
 
